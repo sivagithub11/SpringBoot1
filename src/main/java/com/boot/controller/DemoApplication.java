98030bee2ca4046083896ad381bb5b279d2d58f1
sivagithub11 <siva111prasad@gmail.com>
2020-02-19 20:15:11 +0530

    sadf

--- src/main/java/com/boot/controller/DemoApplication.java
+++ src/main/java/com/boot/controller/DemoApplication.java
@@ -2,10 +2,14 @@ package com.boot.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication extends SpringBootServletInitializer{
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		      return application.sources(DemoApplication.class);
+		   }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
